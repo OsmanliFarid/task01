36b34cf54e35166edcb82f4f78d7312160387a6a
OsmanliFarid <osmanliferid686@gmail.com>
2025-06-16 14:48:40 +0400

    task2

---
+++ task2/app.js
@@ -0,0 +1,15 @@
+import fs from "fs";
+let mesaj = "";
+for (let i = 0; i <= 9; i++) {
+  for (let j = 0; j <= 9; j++) {
+    mesaj += " " + `${i} x ${j} = ${i * j}`;
+    console.log(mesaj);
+  }
+}
+fs.writeFile("./a.txt", mesaj, (err, mesaj) => {
+  if (err) {
+    console.log("ugursuz emeliyat");
+  } else {
+    console.log("ugurlu emeliyat");
+  }
+});
